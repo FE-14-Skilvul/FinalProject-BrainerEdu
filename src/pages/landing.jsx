c17c483c24e67f55f17aedf42201ef20a919c4da
baploarya <aryawardhana7831@gmail.com>
2023-06-04 19:13:05 +0700

    perbaikan

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -16,7 +16,6 @@ const Landing = () => {
                 <ListCourse />
                 <Testimoni />
                 <Contact />
-                <About />
             </Home>
         </>
     )
