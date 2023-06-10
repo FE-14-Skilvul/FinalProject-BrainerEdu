fa14c13dcacc98bb57be236ad36b8734e63e8215
baploarya <aryawardhana7831@gmail.com>
2023-06-11 00:38:46 +0700

    lagi

--- src/section/login.jsx
+++ src/section/login.jsx
@@ -37,8 +37,8 @@ const Login = () => {
             const response = await axios.post(API + '/login', { email: formdata.email, password: formdata.password }, {
                 withCredentials: true, headers: {
                     // Accept: "Origin, X-Requested-With, Content-Type, Accept",
-                    'Access-Control-Allow-Origin': API,
-                    'Access-Control-Request-Headers': "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    'Access-Control-Allow-Origin': "https://finalproject-braineredu-fe14.netlify.app",
+                    'Access-Control-Allow-Headers': "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
                 }
             });
             console.log(response);
