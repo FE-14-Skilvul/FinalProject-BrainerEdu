e1aad1f9d112113467b0baa74449eb018761eba9
Nazwa Septya Salsabilla <nazwasalsabila1002@gmail.com>
2023-06-12 15:23:32 +0700

    perubahan about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -15,7 +15,9 @@ const About = () => {
                 <div className="about__img__box mb-60">
                   <img
                     className="about-main-thumb pl-110"
-                    src="assets/img/slider/01.png"
+                    src="assets/img/slider/about.png"
+                    width={651}
+                    height={721}
                     alt="about-img"
                   />
                   <img
@@ -95,6 +97,189 @@ const About = () => {
           </div>{' '}
         </section>{' '}
         {/*about-us-area end*/}{' '}
+        <section className="course-instructor nav-style-two pt-150 pb-120 pt-md-95 pb-md-70 pt-xs-95 pb-xs-70">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8">
+                <div className="section-title text-center mb-60">
+                  <h2 className="mb-25">Developer</h2>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="z-instructors text-center mb-30">
+                  <div className="z-instructors__thumb mb-15">
+                    <img src="assets/img/slider/arya.png" alt="" />
+                    <div className="social-media">
+                      <a href="#">
+                        <i className="fab fa-linkedin" />
+                      </a>
+                      <a href="#">
+                        <i className="fab fa-twitter" />
+                      </a>
+                      <a href="#">
+                        <i className="fab fa-instagram" />
+                      </a>
+                      <a href="#">
+                        <i className="fab fa-youtube" />
+                      </a>
+                    </div>
+                  </div>
+                  <div className="z-instructors__content">
+                    <h4 className="sub-title mb-10">
+                      <a href="#">Muhammad Arya</a>
+                    </h4>
+                    <p>Front-End Developer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="z-instructors text-center mb-30">
+                  <div className="z-instructors__thumb mb-15">
+                    <img src="assets/img/slider/tajudin.png" alt="" />
+                    <div className="social-media">
+                      <a href="https://www.linkedin.com/in/tajuddin-al-anshori-049596250">
+                        <i className="fab fa-linkedin" />
+                      </a>
+                      <a href="#">
+                        <i className="fab fa-twitter" />
+                      </a>
+                      <a href="https://www.instagram.com/tajuddin.mhmd_">
+                        <i className="fab fa-instagram" />
+                      </a>
+                      <a href="https://youtube.com/@tajuddinalanshori6257">
+                        <i className="fab fa-youtube" />
+                      </a>
+                    </div>
+                  </div>
+                  <div className="z-instructors__content">
+                    <h4 className="sub-title mb-10">
+                      <a href="#">Mochammad Tajudin</a>
+                    </h4>
+                    <p>Front-End Developer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="z-instructors text-center mb-30">
+                  <div className="z-instructors__thumb mb-15">
+                    <img src="assets/img/slider/wawa.png" alt="" />
+                    <div className="social-media">
+                      <a href="https://www.linkedin.com/in/nazwaseptyas">
+                        <i className="fab fa-linkedin" />
+                      </a>
+                      <a href="https://twitter.com/nzwasalsa_">
+                        <i className="fab fa-twitter" />
+                      </a>
+                      <a href="https://www.instagram.com/nazwaw_">
+                        <i className="fab fa-instagram" />
+                      </a>
+                      <a href="#">
+                        <i className="fab fa-youtube" />
+                      </a>
+                    </div>
+                  </div>
+                  <div className="z-instructors__content">
+                    <h4 className="sub-title mb-10">
+                      <a href="#">Nazwa Septya</a>
+                    </h4>
+                    <p>Front-End Developer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="z-instructors text-center mb-30">
+                  <div className="z-instructors__thumb mb-15">
+                    <img src="assets/img/slider/dimas.png" alt="" />
+                    <div className="social-media">
+                      <a href="https://www.linkedin.com/in/dimas-pratomo-mukti-057065263/">
+                        <i className="fab fa-linkedin" />
+                      </a>
+                      <a href="https://twitter.com/muktivip">
+                        <i className="fab fa-twitter" />
+                      </a>
+                      <a href="https://www.instagram.com/dimasvmse_">
+                        <i className="fab fa-instagram" />
+                      </a>
+                      <a href="https://www.youtube.com/@muktivip">
+                        <i className="fab fa-youtube" />
+                      </a>
+                    </div>
+                  </div>
+                  <div className="z-instructors__content">
+                    <h4 className="sub-title mb-10">
+                      <a href="#">Dimas Pratomo</a>
+                    </h4>
+                    <p>Front-End Developer</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="course-categories pt-150 pb-115 pt-md-95 pb-md-65 pt-xs-95 pb-xs-65">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-xl-6 col-lg-8">
+                <div className="section-title section-title-3 text-center mb-60">
+                  <h5 className="mb-25">Challenge Partner</h5>
+                  <h2 className="mb-20">
+                    Skilvul Product Innovation{' '}
+                    <span className="bottom-line">Pendidikan</span>
+                  </h2>
+                  <p>
+                    Membuat dan menemukan solusi digital dalam upaya membantu
+                    masyarakat dan pemerintah untuk mengatasi permasalahan
+                    pendidikan di Indonesia.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row row-cols-1 row-cols-sm-2 online__course__cat">
+              <div className="col">
+                <div
+                  className="courses_link mb-30 wow fadeInUp2 animated"
+                  data-wow-delay=".1s"
+                >
+                  <img
+                    className="icon-01 mb-35"
+                    src="assets/img/logo/skilvul.png"
+                    alt=""
+                  />
+                  <h4 className="sub-title mb-25">Skilvul</h4>
+                  <a href="https://skilvul.com">
+                    <img
+                      className="arrows-icon"
+                      src="assets/img/icon/arrow-right.svg"
+                      alt="arrow-right"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="col">
+                <div
+                  className="courses_link mb-30 wow fadeInUp2 animated"
+                  data-wow-delay=".2s"
+                >
+                  <img
+                    className="icon-01 mb-35"
+                    src="assets/img/logo/km.png"
+                    alt=""
+                  />
+                  <h4 className="sub-title mb-25">Kampus Merdeka</h4>
+                  <a href="https://kampusmerdeka.kemdikbud.go.id/">
+                    <img
+                      className="arrows-icon"
+                      src="assets/img/icon/arrow-right.svg"
+                      alt="arrow-right"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
         <section className="why-chose-us">
           <div className="why-chose-us-bg pt-150 pb-175 pt-md-95 pb-md-90 pt-xs-95 pb-xs-90">
             <div className="container">
@@ -134,11 +319,12 @@ const About = () => {
                         width={530}
                         height={671}
                         alt="Chose-img"
+                        style={{ marginLeft: '25px' }}
                       />
                     </div>{' '}
                     <img
                       className="chose chose_06"
-                      src="assets/img/shape/dot-box-4.svg"
+                      src="assets/img/shape/dot-box3.svg"
                       alt="Chose-img"
                     />
                   </div>{' '}
