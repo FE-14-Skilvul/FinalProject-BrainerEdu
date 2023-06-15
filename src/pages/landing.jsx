0334ed366d203e2a937ada25d316c7cb68ffc348
Nazwa Septya Salsabilla <nazwasalsabila1002@gmail.com>
2023-06-15 15:32:46 +0700

    perbaikan landing

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,29 +1,27 @@
-import Navbar from '../components/navbar'
-import Hero from '../section/hero'
-import KategoriCourse from '../section/kategoriCourse'
-import ListCourse from '../section/listCourse'
-import Footer from '../components/footer'
-import Contact from '../section/contact'
-import Testimoni from '../section/testimoni'
-import Home from '../layout/home'
-import About from '../section/about'
-import Developer from '../components/developer'
-import Partner from '../components/partner'
+import Navbar from '../components/navbar';
+import Hero from '../section/hero';
+import KategoriCourse from '../section/kategoriCourse';
+import ListCourse from '../section/listCourse';
+import Footer from '../components/footer';
+import Contact from '../section/contact';
+import Testimoni from '../section/testimoni';
+import Home from '../layout/home';
+import Developer from '../components/developer';
+import Partner from '../components/partner';
 
 const Landing = () => {
-    return (
-        <>
-            <Home>
-                <Hero />
-                <KategoriCourse />
-                <ListCourse />
-                <Testimoni />
-                <Partner />
-                <Developer />
-                <Contact />
-            </Home>
-        </>
-    )
-}
+  return (
+    <>
+      <Home>
+        <Hero />
+        <Testimoni />
+        <KategoriCourse />
+        <ListCourse />
+        <Partner />
+        <Contact />
+      </Home>
+    </>
+  );
+};
 
-export default Landing
+export default Landing;
